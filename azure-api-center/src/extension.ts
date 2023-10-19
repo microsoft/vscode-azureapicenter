57bdc46e1b0c92e7546e338e077a32acefbc86ad
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-10-19 16:40:44 -0400

    merge pt 2

--- azure-api-center/src/extension.ts
+++ azure-api-center/src/extension.ts
@@ -9,6 +9,7 @@ import { API_CENTER_DESCRIBE_API, API_CENTER_FIND_API, API_CENTER_GENERATE_SNIPP
 // Tree View UI
 import { registerAzureUtilsExtensionVariables } from '@microsoft/vscode-azext-azureutils';
 import { AzExtTreeDataProvider, AzExtTreeItem, IActionContext, createAzExtOutputChannel, registerCommand, registerEvent } from '@microsoft/vscode-azext-utils';
+import { showOpenApi } from './commands/editOpenApi';
 import { exportOpenApi } from './commands/exportOpenApi';
 import { generateApiLibrary } from './commands/generateApiLibrary';
 import { importOpenApi } from './commands/importOpenApi';
@@ -19,7 +20,6 @@ import { ext } from './extensionVariables';
 import { ApiVersionDefinitionTreeItem } from './tree/ApiVersionDefinitionTreeItem';
 import { AzureAccountTreeItem } from './tree/AzureAccountTreeItem';
 import { OpenApiEditor } from './tree/Editors/openApi/OpenApiEditor';
-import { showOpenApi } from './commands/editOpenApi';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "azure-api-center" is now active!');
@@ -66,14 +66,6 @@ export function activate(context: vscode.ExtensionContext) {
                   async (actionContext: IActionContext, doc: vscode.TextDocument) => { await openApiEditor.onDidSaveTextDocument(actionContext, context.globalState, doc); });
 
     registerCommand('azure-api-center.showOpenApi', showOpenApi, doubleClickDebounceDelay);
-
-    registerCommand('azure-api-center.showOpenApi', async (actionContext: IActionContext, node?: ApiVersionDefinitionTreeItem) => {
-        if (!node) {
-            node = <ApiVersionDefinitionTreeItem>await ext.treeDataProvider.showTreeItemPicker(ApiVersionDefinitionTreeItem.contextValue, actionContext);
-        }
-        await openApiEditor.showEditor(node);
-        vscode.commands.executeCommand('setContext', 'isEditorEnabled', true);
-    },              doubleClickDebounceDelay);
 
     registerCommand('azure-api-center.open-api-docs', openApiDocInSwagger);
 
