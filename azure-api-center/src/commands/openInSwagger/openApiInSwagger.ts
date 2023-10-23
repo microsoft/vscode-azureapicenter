78c512828ea4d5d200fb0ea18183a1b75c87e798
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-10-23 10:28:13 -0400

    refined implementation

--- azure-api-center/src/commands/openInSwagger/openApiInSwagger.ts
+++ azure-api-center/src/commands/openInSwagger/openApiInSwagger.ts
@@ -1,31 +1,34 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { IActionContext } from "@microsoft/vscode-azext-utils";
 import * as fse from 'fs-extra';
+import * as path from 'path';
 import { Uri, env } from "vscode";
 import { ext } from "../../extensionVariables";
 import { ApiVersionDefinitionTreeItem } from "../../tree/ApiVersionDefinitionTreeItem";
 import { swaggerTemplate } from "../../tree/Editors/openApi/swaggerTemplate";
 import { createTemporaryFile } from "../../utils/fsUtil";
-import { serve } from "./server";
-import path = require("path");
+import { serve } from "../../utils/server";
 
-const urlPlaceHolder = '{{apiDefinitionTmpFilePath}}';
+// placeholder in the swagger template
+const UrlPlaceHolder = '{{apiDefinitionTmpFilePath}}';
+const SwaggerHtmlFileName = 'index.html';
 
 export async function openAPiInSwagger(context: IActionContext, node: ApiVersionDefinitionTreeItem) {
+    let map = new Map<string, string>();
 
     // write spec to temp file
-    const definitionRaw = await ext.openApiEditor.getData(node);
-    const apiDefinitionTmpFilePath = await createTemporaryFile('/uploads/definition.json');
-    await fse.writeFile(apiDefinitionTmpFilePath, definitionRaw);
-
-    // replace the placeholder in the swagger template with the temp file path
-    const swaggerHtml = swaggerTemplate.replace(urlPlaceHolder, '/uploads/definition.json');
+    const definitionLocalFilePath = await ext.openApiEditor.createTempFileFromTree(node);
+    const definitionLocalFileName = path.basename(definitionLocalFilePath);
+    map.set(`/${definitionLocalFileName}`, definitionLocalFilePath);
 
     // create temp file of the swagger template
-    const htmlFilePath = await createTemporaryFile('index.html');
+    const swaggerHtml = swaggerTemplate.replace(UrlPlaceHolder, definitionLocalFileName);
+    const htmlFilePath = await createTemporaryFile(SwaggerHtmlFileName);
     await fse.writeFile(htmlFilePath, swaggerHtml);
+    map.set('/', htmlFilePath);
 
     // serve the swagger template
-    const address = serve([htmlFilePath, apiDefinitionTmpFilePath]);
+    const address = serve(map);
 
     // open web page
     await env.openExternal(Uri.parse(address));
