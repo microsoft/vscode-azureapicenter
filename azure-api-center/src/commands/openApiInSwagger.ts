2cfcec1ddc3bcc723f258f0499d446ce4cbf1265
alexyaang <59073590+alexyaang@users.noreply.github.com>
2023-10-25 16:54:15 -0400

    removed browser bar

--- azure-api-center/src/commands/openApiInSwagger.ts
+++ azure-api-center/src/commands/openApiInSwagger.ts
@@ -2,7 +2,7 @@
 import { IActionContext } from "@microsoft/vscode-azext-utils";
 import * as fse from 'fs-extra';
 import * as path from 'path';
-import { commands } from "vscode";
+import { ViewColumn, window } from "vscode";
 import { ext } from "../extensionVariables";
 import { ApiVersionDefinitionTreeItem } from "../tree/ApiVersionDefinitionTreeItem";
 import { swaggerTemplate } from "../tree/Editors/openApi/swaggerTemplate";
@@ -32,12 +32,40 @@ export async function openAPiInSwagger(context: IActionContext, node: ApiVersion
     // serve the swagger template
     const address = serve(folderPath, SwaggerHtmlFileName);
 
-    // open web page
-    await commands.executeCommand("simpleBrowser.api.open", address);
+    // create a webview panel to show the swagger template
+    const previewPanel = window.createWebviewPanel(
+        "swaggerPreview",
+        `Swagger Preview - ${definitionFileName}`,
+        ViewColumn.One,
+        {
+          enableScripts: true,
+          retainContextWhenHidden: true,
+        }
+    );
+
+    previewPanel.webview.html = provideTextDocumentContent(address);
+
+    previewPanel.onDidDispose(
+        () =>{},
+        null,
+        ext.context.subscriptions
+      );
 
     // TODO: have a setting to open in browser if user prefers
     // await env.openExternal(Uri.parse(address));
 }
+
+function provideTextDocumentContent(previewUrl: string): string {
+    return `
+			<html>
+				<body style="margin:0px;padding:0px;overflow:hidden">
+					<div style="position:fixed;height:100%;width:100%;">
+					<iframe src="${previewUrl}" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+					</div>
+				</body>
+			</html>
+		`;
+  }
 
 
 
