bfcea1ed73f7ef6ceb5178b49396884bfbe85900
Jun Han <formulahendry@gmail.com>
2023-11-15 09:03:01 +0800

    Fix webpack and build issue (#2)
    * Fix webpack
    
    * remove old vsix

--- azure-api-center/webpack.config.js
+++ azure-api-center/webpack.config.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const path = require('path');
+const terserWebpackPlugin = require('terser-webpack-plugin');
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
@@ -10,7 +11,7 @@ const path = require('path');
 /** @type WebpackConfig */
 const extensionConfig = {
   target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
@@ -45,5 +46,15 @@ const extensionConfig = {
   infrastructureLogging: {
     level: "log", // enables logging required for problem matchers
   },
+  optimization: {
+    minimizer: [
+      new terserWebpackPlugin({
+        terserOptions: {
+          mangle: false,
+          keep_fnames: true
+        }
+      })
+    ]
+  },
 };
-module.exports = [ extensionConfig ];
+module.exports = [extensionConfig];
