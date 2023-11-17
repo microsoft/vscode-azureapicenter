d8e9445cda86b1b9a6939de0a0bb261b363a1c47
Jun Han <formulahendry@gmail.com>
2023-11-17 12:54:48 +0800

    Show 'Open API Documentation' and 'Generate API Client' only on OpenAPI tree item

--- azure-api-center/src/tree/ApiVersionDefinitionTreeItem.ts
+++ azure-api-center/src/tree/ApiVersionDefinitionTreeItem.ts
@@ -3,26 +3,27 @@ import { ApiCenterApiVersionDefinition } from "../azure/ApiCenter/contracts";
 import { treeUtils } from "../utils/treeUtils";
 
 export class ApiVersionDefinitionTreeItem extends AzExtTreeItem {
-    public static contextValue: string = "azureApiCenterApiVersionDefinition";
-    public readonly contextValue: string = ApiVersionDefinitionTreeItem.contextValue;
-    constructor(
-      parent: AzExtParentTreeItem,
-      public apiCenterName: string,
-      public apiCenterApiName: string,
-      public apiCenterApiVersionName: string,
-      public apiCenterApiVersionDefinition: ApiCenterApiVersionDefinition) {
-      super(parent);
-    }
-
-    public get iconPath(): TreeItemIconPath {
-      return treeUtils.getIconPath('definition');
-    }
-
-    public get id(): string {
-      return this.apiCenterApiVersionDefinition.id;
-    }
-
-    public get label(): string {
-      return this.apiCenterApiVersionDefinition.name;
-    }
+  public static contextValue: string = "azureApiCenterApiVersionDefinitionTreeItem";
+  public readonly contextValue: string = ApiVersionDefinitionTreeItem.contextValue;
+  constructor(
+    parent: AzExtParentTreeItem,
+    public apiCenterName: string,
+    public apiCenterApiName: string,
+    public apiCenterApiVersionName: string,
+    public apiCenterApiVersionDefinition: ApiCenterApiVersionDefinition) {
+    super(parent);
+    this.contextValue += "-" + apiCenterApiVersionDefinition.properties.specification.name;
   }
+
+  public get iconPath(): TreeItemIconPath {
+    return treeUtils.getIconPath('definition');
+  }
+
+  public get id(): string {
+    return this.apiCenterApiVersionDefinition.id;
+  }
+
+  public get label(): string {
+    return this.apiCenterApiVersionDefinition.name;
+  }
+}
