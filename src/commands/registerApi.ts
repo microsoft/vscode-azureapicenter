4c483933f3ddcb228f6520db78ae58a51b8de865
Jun Han <formulahendry@gmail.com>
2024-01-25 11:07:57 +0800

    feat: add telemetry

--- src/commands/registerApi.ts
+++ src/commands/registerApi.ts
@@ -1,11 +1,17 @@
 import { IActionContext } from "@microsoft/vscode-azext-utils";
 import * as vscode from 'vscode';
+import { TelemetryClient } from "../common/telemetryClient";
+import { TelemetryEvent, TelemetryProperties } from "../common/telemetryEvent";
 import { RegisterApiOptions } from "../constants";
 import { ApisTreeItem } from "../tree/ApisTreeItem";
 import { registerStepByStep } from "./registerApiSubCommands/registerStepByStep";
 
 export async function registerApi(context: IActionContext, node?: ApisTreeItem) {
     const registerApiOption = await vscode.window.showQuickPick(Object.values(RegisterApiOptions), { title: 'Register API', ignoreFocusOut: true });
+
+    if (registerApiOption) {
+        TelemetryClient.sendEvent(TelemetryEvent.registerApiSelectOption, { [TelemetryProperties.option]: registerApiOption });
+    }
 
     switch (registerApiOption) {
         case RegisterApiOptions.stepByStep:
--- src/common/telemetryEvent.ts
+++ src/common/telemetryEvent.ts
@@ -1,11 +1,13 @@
 export enum TelemetryEvent {
     copilotChat = "copilot-chat",
     treeviewListApiCenters = "treeview.listApiCenters",
+    registerApiSelectOption = "registerApi.selectOption",
 }
 
 export enum TelemetryProperties {
     duration = "duration",
     slashCommand = 'slashCommand',
+    option = 'option',
 };
 
 export enum ErrorProperties {
