6d1c73e9d023bd5f939d160516046284f685d4b4
Jun Han <formulahendry@gmail.com>
2024-02-01 11:28:05 +0800

    fix: use title to show as label, fix context menu not showing in some node

--- src/tree/ApiTreeItem.ts
+++ src/tree/ApiTreeItem.ts
@@ -5,38 +5,38 @@ import { ApiDeploymentsTreeItem } from "./ApiDeploymentsTreeItem";
 import { ApiVersionsTreeItem } from "./ApiVersionsTreeItem";
 
 export class ApiTreeItem extends AzExtParentTreeItem {
-    public static contextValue: string = "azureApiCenterApi";
-    public readonly contextValue: string = ApiTreeItem.contextValue;
-    public readonly apiVersionsTreeItem: ApiVersionsTreeItem;
-    public readonly apiDeploymentsTreeItem: ApiDeploymentsTreeItem;
-    private readonly _apiCenterApi: ApiCenterApi;
-    private readonly _apiCenterName: string;
-    private _nextLink: string | undefined;
-    constructor(parent: AzExtParentTreeItem, apiCenterName: string, apiCenterApi: ApiCenterApi) {
-      super(parent);
-      this._apiCenterName = apiCenterName;
-      this._apiCenterApi = apiCenterApi;
-      this.apiVersionsTreeItem = new ApiVersionsTreeItem(this, apiCenterName, apiCenterApi);
-      this.apiDeploymentsTreeItem = new ApiDeploymentsTreeItem(this, apiCenterName, apiCenterApi);
-    }
-
-    public get iconPath(): TreeItemIconPath {
-      return treeUtils.getIconPath('api');
-    }
-
-    public get id(): string {
-      return this._apiCenterApi.id;
-    }
-
-    public get label(): string {
-      return this._apiCenterApi.name;
-    }
-
-    public hasMoreChildrenImpl(): boolean {
-      return this._nextLink !== undefined;
-    }
-
-    public async loadMoreChildrenImpl(clearCache: boolean, context: IActionContext): Promise<AzExtTreeItem[]> {
-      return [this.apiVersionsTreeItem, this.apiDeploymentsTreeItem]
-    }
+  public static contextValue: string = "azureApiCenterApi";
+  public readonly contextValue: string = ApiTreeItem.contextValue;
+  public readonly apiVersionsTreeItem: ApiVersionsTreeItem;
+  public readonly apiDeploymentsTreeItem: ApiDeploymentsTreeItem;
+  private readonly _apiCenterApi: ApiCenterApi;
+  private readonly _apiCenterName: string;
+  private _nextLink: string | undefined;
+  constructor(parent: AzExtParentTreeItem, apiCenterName: string, apiCenterApi: ApiCenterApi) {
+    super(parent);
+    this._apiCenterName = apiCenterName;
+    this._apiCenterApi = apiCenterApi;
+    this.apiVersionsTreeItem = new ApiVersionsTreeItem(this, apiCenterName, apiCenterApi);
+    this.apiDeploymentsTreeItem = new ApiDeploymentsTreeItem(this, apiCenterName, apiCenterApi);
   }
+
+  public get iconPath(): TreeItemIconPath {
+    return treeUtils.getIconPath('api');
+  }
+
+  public get id(): string {
+    return this._apiCenterApi.id;
+  }
+
+  public get label(): string {
+    return this._apiCenterApi.properties.title;
+  }
+
+  public hasMoreChildrenImpl(): boolean {
+    return this._nextLink !== undefined;
+  }
+
+  public async loadMoreChildrenImpl(clearCache: boolean, context: IActionContext): Promise<AzExtTreeItem[]> {
+    return [this.apiVersionsTreeItem, this.apiDeploymentsTreeItem];
+  }
+}
--- src/tree/ApiVersionTreeItem.ts
+++ src/tree/ApiVersionTreeItem.ts
@@ -1,41 +1,41 @@
 import { AzExtParentTreeItem, AzExtTreeItem, IActionContext, TreeItemIconPath } from "@microsoft/vscode-azext-utils";
-import { ApiCenterApi, ApiCenterApiVersion } from "../azure/ApiCenter/contracts";
+import { ApiCenterApiVersion } from "../azure/ApiCenter/contracts";
 import { treeUtils } from "../utils/treeUtils";
 import { ApiVersionDefinitionsTreeItem } from "./ApiVersionDefinitionsTreeItem";
 
 export class ApiVersionTreeItem extends AzExtParentTreeItem {
-    public static contextValue: string = "azureApiCenterApiVersion";
-    public readonly contextValue: string = ApiVersionTreeItem.contextValue;
-    private readonly _apiCenterApiVersion: ApiCenterApiVersion;
-    public readonly apiVersionDefinitionsTreeItem: ApiVersionDefinitionsTreeItem;
-    private _nextLink: string | undefined;
-    constructor(
-      parent: AzExtParentTreeItem, 
-      apiCenterName: string,
-      apiCenterApiName: string,
-      apiCenterApiVersion: ApiCenterApiVersion) {
-      super(parent);
-      this._apiCenterApiVersion = apiCenterApiVersion;
-      this.apiVersionDefinitionsTreeItem = new ApiVersionDefinitionsTreeItem(this, apiCenterName, apiCenterApiName, apiCenterApiVersion);
-    }
-  
-    public get iconPath(): TreeItemIconPath {
-      return treeUtils.getIconPath('version');
-    }
-  
-    public get id(): string {
-      return this._apiCenterApiVersion.id;
-    }
-  
-    public get label(): string {
-      return this._apiCenterApiVersion.name;
-    }
-
-    public hasMoreChildrenImpl(): boolean {
-      return this._nextLink !== undefined;
-    }
-
-    public async loadMoreChildrenImpl(clearCache: boolean, context: IActionContext): Promise<AzExtTreeItem[]> {
-      return [this.apiVersionDefinitionsTreeItem]
-    }
+  public static contextValue: string = "azureApiCenterApiVersion";
+  public readonly contextValue: string = ApiVersionTreeItem.contextValue;
+  private readonly _apiCenterApiVersion: ApiCenterApiVersion;
+  public readonly apiVersionDefinitionsTreeItem: ApiVersionDefinitionsTreeItem;
+  private _nextLink: string | undefined;
+  constructor(
+    parent: AzExtParentTreeItem,
+    apiCenterName: string,
+    apiCenterApiName: string,
+    apiCenterApiVersion: ApiCenterApiVersion) {
+    super(parent);
+    this._apiCenterApiVersion = apiCenterApiVersion;
+    this.apiVersionDefinitionsTreeItem = new ApiVersionDefinitionsTreeItem(this, apiCenterName, apiCenterApiName, apiCenterApiVersion);
   }
+
+  public get iconPath(): TreeItemIconPath {
+    return treeUtils.getIconPath('version');
+  }
+
+  public get id(): string {
+    return this._apiCenterApiVersion.id;
+  }
+
+  public get label(): string {
+    return this._apiCenterApiVersion.properties.title;
+  }
+
+  public hasMoreChildrenImpl(): boolean {
+    return this._nextLink !== undefined;
+  }
+
+  public async loadMoreChildrenImpl(clearCache: boolean, context: IActionContext): Promise<AzExtTreeItem[]> {
+    return [this.apiVersionDefinitionsTreeItem];
+  }
+}
