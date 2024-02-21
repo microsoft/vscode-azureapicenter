b597aea8462a369a755cb02bb89a3a7595d0845f
wenytang-ms <wenyutang@microsoft.com>
2024-02-21 11:09:46 +0800

    test: update test case

--- test/command/registerApi.test.ts
+++ test/command/registerApi.test.ts
@@ -2,6 +2,7 @@ import { IActionContext } from "@microsoft/vscode-azext-utils";
 import * as sinon from 'sinon';
 import * as vscode from 'vscode';
 import { registerApi } from "../../src/commands/registerApi";
+import * as registerViaCICD from "../../src/commands/registerApiSubCommands/registerViaCICD";
 import { TelemetryClient } from "../../src/common/telemetryClient";
 import { ApisTreeItem } from "../../src/tree/ApisTreeItem";
 
@@ -16,8 +17,10 @@ describe("registerApi", () => {
     it("happy path", async () => {
         sandbox.stub(TelemetryClient, "sendEvent").resolves();
         sandbox.stub(TelemetryClient, "sendErrorEvent").resolves();
-        const showQuickPick = sandbox.stub(vscode.window, "showQuickPick").resolves("stepByStep");
+        const showQuickPick = sandbox.stub(vscode.window, "showQuickPick").resolves("CI/CD");
+        const stubCICD = sandbox.stub(registerViaCICD, "registerViaCICD");
         await registerApi({} as unknown as IActionContext, {} as unknown as ApisTreeItem);
         sandbox.assert.calledOnce(showQuickPick);
+        sandbox.assert.calledOnce(stubCICD)
     })
 })
