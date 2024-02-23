c05b3807519ba774255557134a10fc9343a3b587
wenytang-ms <wenyutang@microsoft.com>
2024-02-23 10:55:23 +0800

    test: update ci

--- src/test/commands/registerApi.test.ts
+++ src/test/commands/registerApi.test.ts
@@ -2,6 +2,7 @@ import { IActionContext } from "@microsoft/vscode-azext-utils";
 import * as sinon from "sinon";
 import * as vscode from "vscode";
 import { registerApi } from "../../commands/registerApi";
+import * as registerStepByStep from "../../commands/registerApiSubCommands/registerStepByStep";
 import * as registerViaCICD from "../../commands/registerApiSubCommands/registerViaCICD";
 import { TelemetryClient } from "../../common/telemetryClient";
 import { ApisTreeItem } from "../../tree/ApisTreeItem";
@@ -12,12 +13,21 @@ describe("registerAPI", () => {
         sandbox.restore();
     })
     it('register API with CICD happy path', async () => {
-        const showQuickPick = sinon.stub(vscode.window, 'showQuickPick').resolves('CI/CD' as any);
+        const showQuickPick = sandbox.stub(vscode.window, 'showQuickPick').resolves('CI/CD' as any);
         sandbox.stub(TelemetryClient, "sendEvent").resolves();
         sandbox.stub(TelemetryClient, "sendErrorEvent").resolves();
-        const stubCICD = sinon.stub(registerViaCICD, 'registerViaCICD').resolves();
+        const stubCICD = sandbox.stub(registerViaCICD, 'registerViaCICD').resolves();
         await registerApi({} as unknown as IActionContext, {} as unknown as ApisTreeItem);
         sandbox.assert.calledOnce(showQuickPick);
         sandbox.assert.calledOnce(stubCICD)
     });
+    it('register API with StepByStep happy path', async () => {
+        const showQuickPick = sandbox.stub(vscode.window, 'showQuickPick').resolves('Step by step' as any);
+        sandbox.stub(TelemetryClient, "sendEvent").resolves();
+        sandbox.stub(TelemetryClient, "sendErrorEvent").resolves();
+        const stubStepByStep = sandbox.stub(registerStepByStep, 'registerStepByStep').resolves();
+        await registerApi({} as unknown as IActionContext, {} as unknown as ApisTreeItem);
+        sandbox.assert.calledOnce(showQuickPick);
+        sandbox.assert.calledOnce(stubStepByStep);
+    })
 });
--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,7 +1,8 @@
 import * as vstest from "@vscode/test-electron";
 import * as cp from "child_process";
-import * as fs from "fs";
+import * as fs from "fs-extra";
 import * as path from 'path';
+import * as semver from "semver";
 let exitCode = 0;
 const cwd = process.cwd();
 const testEnv = Object.create(process.env) as NodeJS.Dict<string>;
@@ -21,8 +22,11 @@ async function runTests(testFolder: string, workspaceFolder: string, logSuffix?:
 	if (!fs.existsSync(logPath))
 		fs.mkdirSync(logPath);
 
-	const codeVersion = (!process.env.BUILD_VERSION || process.env.BUILD_VERSION === "stable") ? "stable" : "insiders";
-
+	const packageContent = await fs.readFile(path.join(__dirname, "..", "..", "..", "package.json"));
+	const packageJSON = JSON.parse(packageContent.toString());
+	let vscodeVersion = packageJSON.engines.vscode;
+	vscodeVersion = semver.minVersion(vscodeVersion)!.version;
+	console.log('Installation vscode version == ', vscodeVersion);
 	// The VS Code download is often flaky on GH Actions, so we want to retry
 	// if required - however we don't want to re-run tests if they fail, so do
 	// the download step separately.
@@ -31,7 +35,8 @@ async function runTests(testFolder: string, workspaceFolder: string, logSuffix?:
 	while (currentAttempt <= maxAttempts) {
 		try {
 			console.log(`Attempting to download VS Code attempt #${currentAttempt}`);
-			const vscodeExecutablePath = await vstest.downloadAndUnzipVSCode();
+
+			const vscodeExecutablePath = await vstest.downloadAndUnzipVSCode(vscodeVersion);
 			const [cliPath, ...args] = vstest.resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
 			console.log('preinstall azure-account extension')
 			cp.spawnSync(cliPath,
@@ -49,7 +54,6 @@ async function runTests(testFolder: string, workspaceFolder: string, logSuffix?:
 		}
 	}
 
-	console.log("Running tests with pre-downloaded VS Code");
 	try {
 		const res = await vstest.runTests({
 			extensionDevelopmentPath: cwd,
@@ -74,7 +78,7 @@ async function runTests(testFolder: string, workspaceFolder: string, logSuffix?:
 				"vscode.github-authentication",
 				"--disable-workspace-trust",
 			],
-			version: codeVersion,
+			version: vscodeVersion,
 		});
 		exitCode = exitCode || res;
 	} catch (e) {
@@ -90,77 +94,11 @@ async function runAllTests(): Promise<void> {
 	testEnv.MOCHA_FORBID_ONLY = "true";
 
 	// Ensure any necessary folders exist.
-	if (!fs.existsSync(".nyc_output"))
-		fs.mkdirSync(".nyc_output");
 	if (!fs.existsSync(".code_test_logs"))
 		fs.mkdirSync(".code_test_logs");
 
 	try {
-		// TODO: Generate this from shared code with generate_launch_configs.ts.
 		await runTests("commands", "test_commands");
-		// if (!process.env.BOT || process.env.BOT === "dart") {
-		// 	await runTests("dart", "hello_world", undefined, { DART_CODE_FORCE_LSP: "false" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "dart_lsp") {
-		// 	await runTests("dart", "hello_world", "lsp", { DART_CODE_FORCE_LSP: "true" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter") {
-		// 	await runTests("flutter", "flutter_hello_world", undefined, { DART_CODE_FORCE_LSP: "false" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter_lsp") {
-		// 	await runTests("flutter", "flutter_hello_world", "lsp", { DART_CODE_FORCE_LSP: "true" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter_snap") {
-		// 	await runTests("flutter_snap", "empty");
-		// }
-		// if (!process.env.BOT || process.env.BOT === "dart_debug") {
-		// 	await runTests("dart_debug", "hello_world", undefined, { DART_CODE_FORCE_SDK_DAP: "false" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "dart_debug_sdk_dap") {
-		// 	await runTests("dart_debug", "hello_world", "sdk_dap", { DART_CODE_FORCE_SDK_DAP: "true" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "dart_web_debug") {
-		// 	await runTests("web_debug", "web");
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter_debug") {
-		// 	await runTests("flutter_debug", "flutter_hello_world", undefined, { DART_CODE_FORCE_SDK_DAP: "false" });
-		// 	await runTests("flutter_bazel", "bazel.code-workspace", undefined, { DART_CODE_FORCE_SDK_DAP: "false" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter_debug_chrome") {
-		// 	await runTests("flutter_debug", "flutter_hello_world", "chrome", { FLUTTER_TEST_DEVICE_ID: "chrome", DART_CODE_FORCE_SDK_DAP: "false" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter_debug_sdk_dap") {
-		// 	await runTests("flutter_debug", "flutter_hello_world", "sdk_dap", { DART_CODE_FORCE_SDK_DAP: "true" });
-		// 	await runTests("flutter_bazel", "bazel.code-workspace", "sdk_dap", { DART_CODE_FORCE_SDK_DAP: "true" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter_debug_chrome_sdk_dap") {
-		// 	await runTests("flutter_debug", "flutter_hello_world", "chrome_sdk_dap", { FLUTTER_TEST_DEVICE_ID: "chrome", DART_CODE_FORCE_SDK_DAP: "true" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter_test_debug") {
-		// 	await runTests("flutter_test_debug", "flutter_hello_world", undefined, { DART_CODE_FORCE_SDK_DAP: "false" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter_test_debug_sdk_dap") {
-		// 	await runTests("flutter_test_debug", "flutter_hello_world", "sdk_dap", { DART_CODE_FORCE_SDK_DAP: "true" });
-		// }
-		// if (!process.env.BOT || process.env.BOT === "misc") {
-		// 	await runTests("dart_create_tests", "dart_create_tests.code-workspace");
-		// 	await runTests("not_activated/dart_create", "empty");
-		// 	await runTests("multi_root", "projects.code-workspace");
-		// 	await runTests("multi_project_folder", "");
-		// 	await runTests("not_activated/flutter_create", "empty");
-		// 	await runTests("flutter_create_tests", "flutter_create_tests.code-workspace");
-		// 	await runTests("dart_nested", "dart_nested");
-		// 	await runTests("dart_nested_flutter", "dart_nested_flutter");
-		// 	await runTests("dart_nested_flutter2", "dart_nested_flutter2");
-		// }
-		// if (!process.env.BOT || process.env.BOT === "flutter_repo") {
-		// 	if (process.env.FLUTTER_REPO_PATH) {
-		// 		await runTests("flutter_repository", process.env.FLUTTER_REPO_PATH);
-		// 	} else {
-		// 		console.error("process.env.FLUTTER_REPO_PATH not set, not running flutter_repo tests");
-		// 		exitCode = 1;
-		// 	}
-		// }
 	} catch (e) {
 		exitCode = 1;
 		console.error(e);
