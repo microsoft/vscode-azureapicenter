d0354ffa8d522b63c4f349c74804180925528809
wenytang-ms <wenyutang@microsoft.com>
2024-03-20 16:30:01 +0800

    test: update

--- integration/basic.test.ts
+++ integration/basic.test.ts
@@ -2,16 +2,15 @@
 // Licensed under the MIT license.
 import { expect, test } from './baseTest';
 
-test('trigger command', async ({ workbox }) => {
+test('trigger generateAPIviaCICD with GitHub', async ({ workbox }) => {
     // trigger command palette.
     await workbox.keyboard.press('Control+Shift+KeyP');
     await workbox.getByRole("combobox", { name: "input" }).fill('>Azure API Center: Register API');
     await workbox.getByRole('listbox').first().press('Enter');
     // select the first option.
-    // await expect(workbox.getByRole('listbox', { name: "CI/CD" })).toHaveCount(2);
     await workbox.getByRole('option', { name: "CI/CD" }).locator('a').click();
     // select the next option.
     await workbox.getByRole('option', { name: 'GitHub' }).locator('a').click();
     // check result.
     await expect(workbox.getByRole('treeitem', { name: 'register-api.yml' })).toHaveCount(1);
-})
+});
