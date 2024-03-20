92a1a4847e7e0fde3b1efa36f26e876f91316ac0
wenytang-ms <wenyutang@microsoft.com>
2024-03-20 15:26:58 +0800

    test: update

--- src/test/e2e/basic.test.ts
+++ src/test/e2e/basic.test.ts
@@ -2,11 +2,20 @@
 // Licensed under the MIT license.
 import { expect, test } from './baseTest';
 
-test('should be able to execute the first test of the example project', async ({ workbox }) => {
-    await workbox.getByRole('treeitem', { name: 'tests', exact: true }).locator('a').click();
-    await workbox.getByRole('treeitem', { name: 'example.spec.ts' }).locator('a').click();
-    await expect(workbox.locator('.testing-run-glyph'), 'there are two tests in the file').toHaveCount(2);
-    await workbox.locator('.testing-run-glyph').first().click();
-    const passedLocator = workbox.locator('.monaco-editor').locator('.codicon-testing-passed-icon');
-    await expect(passedLocator).toHaveCount(1);
-});
+// test('get tree view items', async ({ workbox }) => {
+//     await workbox.getByRole('tab', { name: 'API Center' }).locator('a').click();
+//     const page = await workbox.getByRole('treeitem', { name: 'Sign in to Azure...' }).locator('a').click();
+// })
+
+test('trigger command', async ({ workbox }) => {
+    // trigger command palette.
+    await workbox.keyboard.press('Control+Shift+KeyP');
+    await workbox.getByTitle('Type the name of a command to run.').fill('>Azure API Center: Register API');
+    await workbox.getByRole('listbox').first().press('Enter');
+    // select the first option.
+    await workbox.getByRole('option', { name: "CI/CD" }).locator('a').click();
+    // select the next option.
+    await workbox.getByRole('option', { name: 'GitHub' }).locator('a').click();
+    // check result.
+    await expect(workbox.getByRole('treeitem', { name: 'register-api.yml' })).toHaveCount(1);
+})
