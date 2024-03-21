7382c13af9c6b65721f01090be344e2597334be6
Jun Han <formulahendry@gmail.com>
2024-03-21 09:39:54 +0800

    fix: adopt latest changes of Copilot API (#81)
    * fix: adopt latest changes of Copilot API
    
    * fix: use VS Code insiders in unit test

--- src/extension.ts
+++ src/extension.ts
@@ -97,23 +97,8 @@ export async function activate(context: vscode.ExtensionContext) {
     registerCommandWithTelemetry('azure-api-center.apiCenterTreeView.refresh', async (context: IActionContext) => refreshTree(context));
 
     const agent = vscode.chat.createChatParticipant('apicenter', handleChatMessage);
-    agent.description = 'Build, discover, and consume great APIs.';
-    agent.commandProvider = {
-        provideCommands(token) {
-            return [
-                {
-                    name: 'list',
-                    description: 'List available APIs.',
-                },
-                {
-                    name: 'find',
-                    description: 'Find an API given a search query.',
-                }
-            ];
-        },
-    };
     agent.followupProvider = {
-        provideFollowups(result: IChatResult, token: vscode.CancellationToken) {
+        provideFollowups(result: IChatResult, context: vscode.ChatContext, token: vscode.CancellationToken) {
             if (result.metadata.command === 'list') {
                 return [{
                     prompt: '$more',
