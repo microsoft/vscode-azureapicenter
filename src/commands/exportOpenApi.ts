52d6b631189b7062047f5ba87ccf2840e53a9792
wenytang-ms <wenyutang@microsoft.com>
2024-03-27 16:41:34 +0800

    feat: export open api

--- src/commands/exportOpenApi.ts
+++ src/commands/exportOpenApi.ts
@@ -1,26 +1,46 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import { IActionContext } from "@microsoft/vscode-azext-utils";
-import { ApiVersionDefinitionsTreeItem } from "../tree/ApiVersionDefinitionsTreeItem";
-import { OpenDialogOptions, ProgressLocation, Uri, window, workspace } from "vscode";
-import { ext } from "../extensionVariables";
-import * as fse from 'fs-extra';
-import { ApiCenterService } from "../azure/ApiCenter/ApiCenterService";
 import { getResourceGroupFromId } from "@microsoft/vscode-azext-azureutils";
+import { IActionContext } from "@microsoft/vscode-azext-utils";
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as vscode from "vscode";
+import { ApiCenterService } from "../azure/ApiCenter/ApiCenterService";
 import { ApiVersionDefinitionTreeItem } from "../tree/ApiVersionDefinitionTreeItem";
-import { ApiCenterApiVersionDefinitionImport } from "../azure/ApiCenter/contracts";
+import { createTemporaryFolder } from "../utils/fsUtil";
 
 export async function exportOpenApi(
-    context: IActionContext, 
+    context: IActionContext,
     node?: ApiVersionDefinitionTreeItem): Promise<void> {
 
-        const apiCenterService = new ApiCenterService(
-            node?.subscription!, 
-            getResourceGroupFromId(node?.id!), 
-            node?.apiCenterName!);   
+    const apiCenterService = new ApiCenterService(
+        node?.subscription!,
+        getResourceGroupFromId(node?.id!),
+        node?.apiCenterName!);
 
-        const exportedSpec = await  apiCenterService.exportSpecification(
-                node?.apiCenterApiName!, 
-                node?.apiCenterApiVersionName!, 
-                node?.apiCenterApiVersionDefinition.name!); 
+    const exportedSpec = await apiCenterService.exportSpecification(
+        node?.apiCenterApiName!,
+        node?.apiCenterApiVersionName!,
+        node?.apiCenterApiVersionDefinition.name!);
+
+    await writeToHttpFile(node!, exportedSpec.format, exportedSpec.value);
+}
+
+function getFolderName(treeItem: ApiVersionDefinitionTreeItem): string {
+    return `${treeItem.apiCenterName}-${treeItem.apiCenterApiName}`;
+}
+
+function getFilename(treeItem: ApiVersionDefinitionTreeItem): string {
+    return `${treeItem.apiCenterApiVersionName}-tempFile.http`;
+}
+
+async function writeToHttpFile(node: ApiVersionDefinitionTreeItem, httpFileContent: string, value: string) {
+    const folderName = getFolderName(node);
+    const folderPath = await createTemporaryFolder(folderName);
+    const fileName = getFilename(node);
+    const localFilePath: string = path.join(folderPath, fileName);
+    await fs.ensureFile(localFilePath);
+    const document: vscode.TextDocument = await vscode.workspace.openTextDocument(localFilePath);
+    await vscode.workspace.fs.writeFile(vscode.Uri.file(localFilePath), Buffer.from(value));
+    await vscode.window.showTextDocument(document);
 }
