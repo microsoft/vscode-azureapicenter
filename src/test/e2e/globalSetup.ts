464c0c9d4d8a5d26fdb12d5b67a5dd776b4df60f
wenytang-ms <wenyutang@microsoft.com>
2024-04-17 16:08:49 +0800

    test: update

--- src/test/e2e/globalSetup.ts
+++ src/test/e2e/globalSetup.ts
@@ -1,14 +1,15 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 import { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath } from '@vscode/test-electron';
-import { spawnSync } from 'child_process';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
 
 export default async () => {
     const vscodePath = await downloadAndUnzipVSCode('insiders');
     const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodePath);
-    spawnSync(cli, [...args, '--install-extension', 'ms-vscode.azure-account'], {
+    await execAsync(`${cli} ${args[0]} ${args[1]} --install-extension ms-vscode.azure-account`, {
         encoding: 'utf-8',
-        stdio: 'inherit',
-    });
-    // await downloadAndUnzipVSCode('stable');
+    })
 };
