805f5599b317d7860233665745f0fd1a985335b7
wenyutang <wenyutang@microsoft.com>
2024-05-10 21:03:27 +0800

    test: update

--- src/test/e2e/tests/registerApiViaCICD.test.ts
+++ src/test/e2e/tests/registerApiViaCICD.test.ts
@@ -1,12 +1,17 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
+import os from "os";
 import { expect, test } from '../baseTest';
 
 test('trigger generateAPIviaCICD with GitHub', async ({ workbox }) => {
     // wait API Center extension installed on VS Code.
     await workbox.getByRole("tab", { name: "API Center" }).isVisible();
     // trigger command palette.
-    await workbox.keyboard.press('Control+Shift+KeyP');
+    if (os.platform() === 'darwin') {
+        await workbox.keyboard.press('Meta+Shift+P');
+    } else {
+        await workbox.keyboard.press('Control+Shift+KeyP');
+    }
     await workbox.getByRole("combobox", { name: "INPUT" }).fill('>Azure API Center: Register API');
     await workbox.getByRole('listbox').first().press('Enter');
     // select the first option.
@@ -21,7 +26,11 @@ test('trigger generateAPIviaCICD with Azure DevOps', async ({ workbox }) => {
     // wait API Center extension installed on VS Code.
     await workbox.getByRole("tab", { name: "API Center" }).isVisible();
     // trigger command palette.
-    await workbox.keyboard.press('Control+Shift+KeyP');
+    if (os.platform() === 'darwin') {
+        await workbox.keyboard.press('Meta+Shift+P');
+    } else {
+        await workbox.keyboard.press('Control+Shift+KeyP');
+    }
     await workbox.getByRole("combobox", { name: "INPUT" }).fill('>Azure API Center: Register API');
     await workbox.getByRole('listbox').first().press('Enter');
     // select the first option.
