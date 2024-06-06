3bdc8d949828ec04b13c2d77a2b3216e368f7e78
Jun Han <formulahendry@gmail.com>
2024-06-06 13:47:34 +0800

    feat: return YAML format, handle no REST API code

--- src/commands/generateApiFromCode.ts
+++ src/commands/generateApiFromCode.ts
@@ -26,7 +26,8 @@ export async function generateApiFromCode(context: IActionContext) {
             vscode.LanguageModelChatMessage.User(`You are an expert in ${languageId} programming language and OpenAPI.
 Generate the OpenAPI Specification from the provided ${languageId} programming language.
 Try your best to parse the code and understand the code structure.
-Only return the specification content, without any additional information.
+Only return the specification content with YAML format, without any additional information.
+If the code is not REST API related, return "Not REST API related code" and provide an explanation.
 If this task can't be completed, return "Sorry, I can't assist" and provide an explanation.
 Here's the ${languageId} code of Web API:
 \`\`\`
