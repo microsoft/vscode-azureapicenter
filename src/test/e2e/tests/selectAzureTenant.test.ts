dd4eccee3e0966f00aba604dbe581ab4ae35c863
Ivan_Chen <v-ivanchen@microsoft.com>
2024-07-26 11:04:44 +0800

    test: debug

--- src/test/e2e/tests/selectAzureTenant.test.ts
+++ src/test/e2e/tests/selectAzureTenant.test.ts
@@ -10,16 +10,22 @@ test('select Tenant', async ({ workbox }) => {
     // wait API Center extension installed on VS Code.
     expect(await VscodeOperator.isSideTabItemExist(workbox, VSCode.TAB_API_CENTER)).toBeTruthy();
     await VscodeOperator.activeSideTab(workbox, VSCode.TAB_API_CENTER, Timeout.PREPARE_EXT);
+    console.log('select tenant: ', APICenter.SELECT_TENANT);
     // select tenant
     expect(await VscodeOperator.isTreeItemExist(workbox, APICenter.SELECT_TENANT)).toBeTruthy();
+    console.log('click tenant: ', APICenter.SELECT_TENANT);
     await VscodeOperator.clickTreeItem(workbox, APICenter.SELECT_TENANT);
+    console.log('select tenant name: ', TestENV.AZURE_TENANT_NAME);
     await VscodeOperator.selectOptionByName(workbox, TestENV.AZURE_TENANT_NAME!);
     // check subscription
+    console.log('check subscription');
     const isSelectSubsExist = await VscodeOperator.isTreeItemExist(workbox, APICenter.SELECT_SUBS);
     if (isSelectSubsExist) {
         // select subscription check all
+        console.log('check all: ', APICenter.SELECT_SUBS);
         await VscodeOperator.clickTreeItem(workbox, APICenter.SELECT_SUBS);
         await VscodeOperator.checkallCheckbox(workbox);
     }
+    console.log('click subscription: ', TestENV.AZURE_SUBSCRIPTION_NAME!);
     expect(await VscodeOperator.isTreeItemExist(workbox, TestENV.AZURE_SUBSCRIPTION_NAME!)).toBeTruthy();
 });
