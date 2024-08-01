a2707ab69fd8073a9abbe7fda37c7c4e6f9ace94
wenyutang-ms <wenyutang@microsoft.com>
2024-08-01 13:02:36 +0800

    feat: add feautres

--- src/azure/ApiCenter/ApiCenterDistinct.ts
+++ src/azure/ApiCenter/ApiCenterDistinct.ts
@@ -2,18 +2,22 @@
 // Licensed under the MIT license.
 import { ApiCenter, ApiCenterApi, ApiCenterApiVersion, ApiCenterApiVersionDefinition, GeneralApiCenter, GeneralApiCenterApi, GeneralApiCenterApiVersion, GeneralApiCenterApiVersionDefinition } from "./contracts";
 
-export function isApiCenterServiceManagement(obj: GeneralApiCenter): obj is ApiCenter {
+function checkObj(obj: any): boolean {
     return obj && 'id' in obj && 'properties' in obj && typeof obj.properties == 'object';
+}
+
+export function isApiCenterServiceManagement(obj: GeneralApiCenter): obj is ApiCenter {
+    return checkObj(obj);
 }
 
 export function isApiCenterManagement(obj: GeneralApiCenterApi): obj is ApiCenterApi {
-    return obj && 'id' in obj && 'properties' in obj && typeof obj.properties == 'object';
+    return checkObj(obj);
 }
 
 export function isApiCenterVersionManagement(obj: GeneralApiCenterApiVersion): obj is ApiCenterApiVersion {
-    return obj && 'id' in obj && 'properties' in obj && typeof obj.properties == 'object';
+    return checkObj(obj);
 }
 
 export function isApiCenterVersionDefinitionManagement(obj: GeneralApiCenterApiVersionDefinition): obj is ApiCenterApiVersionDefinition {
-    return obj && 'id' in obj && 'properties' in obj && typeof obj.properties == 'object';
+    return checkObj(obj);
 }
