d647b63b640573f329b4900e99a781414ef0e6c9
Ivan_Chen <v-ivanchen@microsoft.com>
2024-08-06 07:55:43 +0800

    test: change case name

--- src/test/e2e/tests/validateSubscription.test.ts
+++ src/test/e2e/tests/validateSubscription.test.ts
@@ -5,11 +5,10 @@ import { expect, test } from '../baseTest';
 import { APICenter, TestENV, Timeout, VSCode } from '../utils/constants';
 import VscodeOperator from '../utils/vscodeOperator';
 
-test('select Tenant', async ({ workbox }) => {
+test('validate subscription', async ({ workbox }) => {
     await workbox.waitForTimeout(Timeout.PREPARE_TEST);
     // wait API Center extension installed on VS Code.
     expect(await VscodeOperator.isSideTabItemExist(workbox, "API Center")).toBeTruthy();
     await VscodeOperator.activeSideTab(workbox, VSCode.TAB_API_CENTER, Timeout.PREPARE_EXT);
-    console.log('click subscription: ', TestENV.AZURE_SUBSCRIPTION_NAME!);
     expect(await VscodeOperator.isTreeItemExist(workbox, "Teams Cloud - E2E Testing with TTL = 1 Days")).toBeTruthy();
 });
