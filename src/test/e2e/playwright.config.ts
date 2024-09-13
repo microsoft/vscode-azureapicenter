3eefbfc5fed2f27a5d66e2c1e0d8b340b995cf01
Ivan_Chen <v-ivanchen@microsoft.com>
2024-09-13 15:56:52 +0800

    test: add open api documentation test case

--- src/test/e2e/playwright.config.ts
+++ src/test/e2e/playwright.config.ts
@@ -6,7 +6,7 @@ import { TestOptions } from './baseTest';
 export default defineConfig<void, TestOptions>({
     reporter: process.env.CI ? [['json', { outputFile: '../../../test-results/results.json' }]] : 'list',
     timeout: 60_000,
-    workers: 1,
+    workers: 2,
     expect: {
         timeout: 30_000,
     },
