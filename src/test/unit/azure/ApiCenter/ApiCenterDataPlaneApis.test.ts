7c0db143e5a754e65b33725fb69e50613fcb015b
wenyutang-ms <wenyutang@microsoft.com>
2024-10-08 13:54:06 +0800

    test: add test cases

---
+++ src/test/unit/azure/ApiCenter/ApiCenterDataPlaneApis.test.ts
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import { HttpOperationResponse, ServiceClient } from "@azure/ms-rest-js";
+import { ISubscriptionContext } from "@microsoft/vscode-azext-utils";
+import * as assert from "assert";
+import * as sinon from "sinon";
+import { ApiCenterDataPlaneService } from "../../../../azure/ApiCenter/ApiCenterDataPlaneAPIs";
+describe("Api Center Data Plane Apis Server", () => {
+    let sandbox: sinon.SinonSandbox;
+    let subscriptionContext: ISubscriptionContext;
+    before(() => {
+        sandbox = sinon.createSandbox();
+    });
+    beforeEach(() => {
+        subscriptionContext = {
+            credentials: {
+                getToken: sandbox.stub().resolves({ token: 'fake-token' })
+            }
+        } as unknown as ISubscriptionContext;
+    });
+    afterEach(() => {
+        sandbox.restore();
+    });
+    it("get apis center apis succeeded", async () => {
+        const mockResponse = {
+            status: 200,
+            parsedBody: { value: "fakeApis", nextLink: "fakeNextLink" },
+        } as HttpOperationResponse;
+        const sendRequestStub = sandbox.stub(ServiceClient.prototype, "sendRequest");
+        sendRequestStub.onSecondCall().resolves(mockResponse);
+
+        const serverClient = new ApiCenterDataPlaneService(subscriptionContext);
+
+        const response = await serverClient.getApiCenterApis();
+
+        assert.strictEqual(response.value, 'fakeApis');
+    });
+    it("get apis center apis failed", async () => {
+        sandbox.stub(ServiceClient.prototype, "sendRequest").resolves({ status: 500, bodyAsText: "error" } as HttpOperationResponse);
+
+        const serverClient = new ApiCenterDataPlaneService(subscriptionContext);
+
+        const response = await serverClient.getApiCenterApis();
+
+        assert.strictEqual(response.value, undefined);
+    });
+});
