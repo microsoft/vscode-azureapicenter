e269ebf39bb10d592f1b45b35aa9d40f54eb4832
wenyutang <wenyutang@microsoft.com>
2024-10-15 21:07:08 +0800

    test: fix e2e test case

--- src/test/e2e/globalSetup.ts
+++ src/test/e2e/globalSetup.ts
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 import { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath } from '@vscode/test-electron';
-
+import * as semver from "semver";
+const config = require('../../../package.json');
+const vscodeVer = config.engines.vscode;
+const vscodeVersion = semver.minVersion(vscodeVer)!.version;
 export default async () => {
-    const vscodePath = await downloadAndUnzipVSCode('insiders');
+    const vscodePath = await downloadAndUnzipVSCode(vscodeVersion!);
     resolveCliArgsFromVSCodeExecutablePath(vscodePath);
 };
