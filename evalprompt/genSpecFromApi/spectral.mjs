57549a636652e29645d5371a8bb9017c53d84a3c
wenyutang <wenyutang@microsoft.com>
2025-01-07 10:03:23 +0800

    ci: update

--- evalprompt/genSpecFromApi/spectral.mjs
+++ evalprompt/genSpecFromApi/spectral.mjs
@@ -1,23 +1,19 @@
 const { Spectral } = require("@stoplight/spectral-core")
-import { truthy } from "@stoplight/spectral-functions";
+import { bundleAndLoadRuleset } from "@stoplight/spectral-ruleset-bundler/with-loader";
+import { fetch } from "@stoplight/spectral-runtime";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
 
 export default async function (output, context) {
     text = output.replace(/^```yaml.*\n/, '');
     text = text.replace(/\n```$/, '');
     const spectral = new Spectral();
-    spectral.setRuleset({
-        rules: {
-            "no-empty-description": {
-                given: "$..description",
-                message: "Description must not be empty",
-                then: {
-                    function: truthy,
-                },
-            },
-        },
-    });
-    const res = await spectral.run(text);
-    console.log('------', res);
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const rulesetFilepath = path.join(__dirname, "spectral.yml");
+    spectral.setRuleset(await bundleAndLoadRuleset(rulesetFilepath, { fs, fetch }));
+
+    await spectral.run(text);
     return {
         pass: true,
         score: 0.5,
