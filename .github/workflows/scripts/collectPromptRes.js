ec199bbc08a7ace278a9cdc1064db174419b582c
wenyutang <wenyutang@microsoft.com>
2025-01-08 13:13:38 +0800

    chore: update

---
+++ .github/workflows/scripts/collectPromptRes.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+
+const evalPromptDir = path.join(__dirname, "..", "..", "..", "evalprompt");
+// collect results
+let commentContent = '';
+const directories = fs.readdirSync(evalPromptDir, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+for (const dir of directories) {
+    const outputFile = path.join(path.join(evalPromptDir, dir), 'output.json');
+    if (outputFile == null || !fs.existsSync(outputFile))
+        continue;
+    const output = JSON.parse(fs.readFileSync(outputFile, 'utf8'));
+    let body = `⚠️ LLM prompt was modified in these files: ${dir}
+
+    | Success | Failure |
+    |---------|---------|
+    | ${output.results.stats.successes}      | ${output.results.stats.failures}       |
+
+    `;
+    if (output.shareableUrl) {
+        body = body.concat(`**» [View eval results](${output.shareableUrl}) «**`);
+    } else {
+        body = body.concat('**» View eval results in CI console «**');
+    }
+    commentContent += body + "\n\n";
+}
+
+fs.writeFileSync(path.join(process.cwd(), 'commentContent.txt'), commentContent);
