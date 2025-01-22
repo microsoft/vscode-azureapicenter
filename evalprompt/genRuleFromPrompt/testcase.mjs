de4181237a5c780bf1e751a5a5514b82a14d4a76
wenyutang <wenyutang@microsoft.com>
2025-01-22 14:39:09 +0800

    test: adjust the prompt and add openapi spec file for evaluation, and adjust evaluation methods

--- evalprompt/genRuleFromPrompt/testcase.mjs
+++ evalprompt/genRuleFromPrompt/testcase.mjs
@@ -34,26 +34,49 @@ export default async function (output, context) {
             }
         }
     }
-    const myDocument = new Document(
-        fs.readFileSync(path.join(__dirname, "apispec", "petstore.yaml"), "utf8"),
-        Yaml
-    )
+
     const spectral = new Spectral();
     spectral.setRuleset(ymlObjs);
-    const res = await spectral.run(myDocument);
     keywords = context.config.keywords;
-    for (let item of res) {
-        if (keywords.some(r => item.message.includes(r))) {
-            return {
-                pass: true,
-                score: 1,
-                reason: `Hit the critical field ${keywords} in ${item.message}`,
+    const componentsRes = [];
+    let isSuccess = false;
+    const files = fs.readdirSync(path.join(__dirname, "apispec"));
+    for (let file of files) {
+        const myDocument = new Document(
+            fs.readFileSync(path.join(__dirname, "apispec", file), "utf8"),
+            Yaml
+        );
+        const res = await spectral.run(myDocument);
+        for (let item of res) {
+            if (keywords.some(r => item.message.includes(r))) {
+                isSuccess = true;
+                componentsRes.push({
+                    pass: true,
+                    score: 1,
+                    reason: `Hit the critical field ${keywords} in ${item.message} from file ${file}`,
+                });
+            } else {
+                componentsRes.push({
+                    pass: false,
+                    score: 0,
+                    reason: `No critical field was hit with in ${item.message} from file ${file}`,
+                });
             }
+        }
+    }
+    const passCount = componentsRes.filter(item => item.pass).length;
+    if (isSuccess) {
+        return {
+            pass: true,
+            score: passCount / componentsRes.length,
+            reason: `Hit the critical field ${keywords}`,
+            componentResults: componentsRes
         }
     }
     return {
         pass: false,
         score: 0,
         reason: `No critical field was hit with ${keywords}`,
+        componentsRes: componentsRes
     }
 }
