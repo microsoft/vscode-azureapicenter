07a9a6e61cc1befeb451f3b0fbcdca59d54150ab
wenyt <75360946+wenytang-ms@users.noreply.github.com>
2025-02-06 19:17:43 +0800

    test: add evaluation for user prompt to generate spectral rule (#309)
    * test: prompt evaluation
    
    * test: add evaluation for relavance evaluation
    
    * test; remove files
    
    * test: restore file
    
    * test: break down evaluation cases
    
    * test: adjust the prompt and add openapi spec file for evaluation, and adjust evaluation methods
    
    * test: update
    
    * test: update
    
    * test: update
    
    * test: update
    
    * test: update
    
    * test: update
    
    * test: update
    
    * test: update
    
    * test: update base line
    
    * test: update the logic for validation
    
    * test: update validation
    
    * test: update
    
    * test: update

--- .github/workflows/scripts/collectPromptRes.js
+++ .github/workflows/scripts/collectPromptRes.js
@@ -9,22 +9,32 @@ const directories = fs.readdirSync(evalPromptDir, { withFileTypes: true })
     .map(dirent => dirent.name);
 console.log(directories);
 for (const dir of directories) {
-    const outputFile = path.join(path.join(evalPromptDir, dir), 'output.json');
+    const outputFile = path.join(evalPromptDir, dir, 'output.json');
+    const baselineFile = path.join(evalPromptDir, dir, 'baseline.json');
     if (outputFile == null || !fs.existsSync(outputFile))
         continue;
     console.log(outputFile);
     const output = JSON.parse(fs.readFileSync(outputFile, 'utf8'));
-    let body = `⚠️ LLM prompt result for ${dir}
+    let body = ` LLM prompt result for ${dir}
 
-| Success | Failure |
-|---------|---------|
-| ${output.results.stats.successes}      | ${output.results.stats.failures}       |
-
+| From | Success | Failure |  Score  |
+|---------|---------|---------|---------|
+| Output | ${output.results.stats.successes} | ${output.results.stats.failures} | ${output.results.prompts[0].metrics.score} |
 `;
+    if (baselineFile && fs.existsSync(baselineFile)) {
+        const baseline = JSON.parse(fs.readFileSync(baselineFile, 'utf8'));
+        body += `|Baseline| ${baseline.success} | ${baseline.failure} | ${baseline.score} |
+        `
+        if (baseline.successes < output.results.stats.successes || baseline.failures > output.results.stats.failures || baseline.score > output.results.prompts[0].metrics.score) {
+            body = `[Not PASS]🚨 ` + body;
+        } else {
+            body = `[PASS]✅ ` + body;
+        }
+    }
     if (output.shareableUrl) {
-        body = body.concat(`**» [View eval results](${output.shareableUrl}) «**`);
+        body = body.concat(`\n**» [View eval results](${output.shareableUrl}) «**\n`);
     } else {
-        body = body.concat('**» View eval results in CI console «**');
+        body = body.concat('\n**» View eval results in CI console «**\n');
     }
     commentContent += body + "\n\n";
 }
