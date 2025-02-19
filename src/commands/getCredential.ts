6adda0a4899f7634d6c0bbfadd6e42a6de6c28a8
Jun Han <junhan@microsoft.com>
2025-02-19 12:21:24 +0800

    feat: show full credential info

--- src/commands/getCredential.ts
+++ src/commands/getCredential.ts
@@ -7,12 +7,18 @@ import { ApiCenterService } from "../azure/ApiCenter/ApiCenterService";
 import { ApiAccessTreeItem } from "../tree/ApiAccessTreeItem";
 
 export async function getCredential(context: IActionContext, node: ApiAccessTreeItem) {
-    vscode.window.showInformationMessage(`Getting credential for ${node.apiCenterApiAccess.name}`);
+    // vscode.window.showInformationMessage(`Getting credential for ${node.apiCenterApiAccess.name}`);
 
     const resourceGroupName = getResourceGroupFromId(node.id);
     const apiCenterService = new ApiCenterService(node.parent?.subscription!, resourceGroupName, node.apiCenterName);
 
     const credential = await apiCenterService.getApiCenterApiCredential(node.apiCenterApiName, node.apiCenterApiVersionName, node.apiCenterApiAccess.name);
 
-    vscode.window.showInformationMessage(`Credential for ${node.apiCenterApiAccess.name} is ${credential.apiKey?.value}`);
+    // vscode.window.showInformationMessage(`Credential for ${node.apiCenterApiAccess.name} is ${credential.apiKey?.value}`);
+
+    const document = await vscode.workspace.openTextDocument({
+        content: JSON.stringify(credential, null, 2),
+        language: 'json'
+    });
+    await vscode.window.showTextDocument(document);
 }
