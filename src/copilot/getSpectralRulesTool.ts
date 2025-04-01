0e056f2c6521f3fd80597d0088d68601d8b82ad5
Jun Han <junhan@microsoft.com>
2025-04-01 14:28:17 +0800

    feat: add telemetry

--- src/copilot/getSpectralRulesTool.ts
+++ src/copilot/getSpectralRulesTool.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 import * as vscode from 'vscode';
+import { TelemetryClient } from '../common/telemetryClient';
+import { TelemetryEvent } from '../common/telemetryEvent';
 import { GenerateOpenApi } from './generateOpenApi';
 
 export class GetSpectralRulesTool implements vscode.LanguageModelTool<void> {
@@ -8,6 +10,7 @@ export class GetSpectralRulesTool implements vscode.LanguageModelTool<void> {
         _options: vscode.LanguageModelToolInvocationOptions<void>,
         _token: vscode.CancellationToken
     ) {
+        TelemetryClient.sendEvent(TelemetryEvent.getSpectralRulesToolInvoke);
         const rulesetFile = GenerateOpenApi.getRulesetFile();
         const ruleDescriptions = await GenerateOpenApi.getRuleDescriptions(rulesetFile);
 
@@ -18,6 +21,7 @@ export class GetSpectralRulesTool implements vscode.LanguageModelTool<void> {
         _options: vscode.LanguageModelToolInvocationPrepareOptions<void>,
         _token: vscode.CancellationToken
     ) {
+        TelemetryClient.sendEvent(TelemetryEvent.getSpectralRulesToolPrepareInvocation);
         return {
             invocationMessage: `@apicenter: Getting Spectral rules`,
         };
