3ddcdfe3e4891991a6557b5dd1d4a638c410e9ec
Jun Han <junhan@microsoft.com>
2025-04-29 16:06:59 +0800

    feat: mask the credential value

--- src/copilot/tools/getCredential.ts
+++ src/copilot/tools/getCredential.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 import * as vscode from 'vscode';
+import { ext } from '../../extensionVariables';
+import { UiStrings } from '../../uiStrings';
 import { TelemetryUtils } from '../../utils/telemetryUtils';
 import { createApiCenterDataPlaneService } from '../utils/dataPlaneUtil';
 
@@ -18,9 +20,36 @@ export class GetCredentialTool implements vscode.LanguageModelTool<IGetCredentia
         return TelemetryUtils.callLmToolWithTelemetry('lmTool.getCredential', async () => {
             const apiCenterDataPlaneService = await createApiCenterDataPlaneService();
 
-            const response = await apiCenterDataPlaneService.getCredential(options.input.apiName, options.input.apiVersionName, options.input.authenticationName);
+            const credential = await apiCenterDataPlaneService.getCredential(options.input.apiName, options.input.apiVersionName, options.input.authenticationName);
 
-            return new vscode.LanguageModelToolResult([new vscode.LanguageModelTextPart(JSON.stringify(response, null, 2))]);
+            let languageModelText = "";
+            const originalCredential = JSON.parse(JSON.stringify(credential));
+
+            if (credential && credential.securityScheme) {
+                let credentialValue = "";
+                // Mask the credential value to avoid exposing sensitive information to LLM
+                if (credential.apiKey?.value) {
+                    credentialValue = credential.apiKey.value;
+                    credential.apiKey.value = "******";
+                } else if (credential.oauth2?.clientSecret) {
+                    credentialValue = credential.oauth2.clientSecret;
+                    credential.oauth2.clientSecret = "******";
+                }
+
+                if (credentialValue) {
+                    await vscode.env.clipboard.writeText(credentialValue);
+                    ext.outputChannel.appendLine(vscode.l10n.t(UiStrings.CredentialFor, options.input.authenticationName));
+                    ext.outputChannel.appendLine(JSON.stringify(originalCredential, null, 2));
+
+                    languageModelText = `Do let user know the credential for '${options.input.authenticationName}' has been copied to clipboard.
+User could also view the full credential details in the 'Azure API Center' Output Channel.
+The schema of credential (credential value is masked) is:\n${JSON.stringify(credential, null, 2)}`;
+                } else {
+                    languageModelText = JSON.stringify(credential, null, 2);
+                }
+            }
+
+            return new vscode.LanguageModelToolResult([new vscode.LanguageModelTextPart(languageModelText)]);
         });
     }
 }
